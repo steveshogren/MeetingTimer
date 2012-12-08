15f80a1caef68e324cc5c14459a468493c47b13c
Steve <madklowns@gmail.com>
2012-12-08 09:34:16 -0500

    jquerying the clock updater

--- javascript.js
+++ javascript.js
@@ -1,59 +1,62 @@
-
-
 $(document).ready(function() {
-    var minNumber = 1;
-    var maxNumber = 10;
+    var minNumber = 1,
+        maxNumber = 20,
+        secondsRaw = 0,
+        people = 1,
+        skipThese = [],
+        roundToTens = function(num) {
+            return (num < 10 ? "0" : "") + num;
+        },
+        updateTimer = function() {
+            secondsRaw = secondsRaw + people;
+            var hours = Math.floor(secondsRaw / (60 * 60)),
+                divisor_for_minutes = roundToTens(secondsRaw % (60 * 60)),
+                minutes = roundToTens(Math.floor(divisor_for_minutes / 60)),
+                divisor_for_seconds = roundToTens(divisor_for_minutes % 60),
+                seconds = roundToTens(Math.ceil(divisor_for_seconds));
 
-    var skipThese = [];
-
-    for (var i = minNumber; i <= maxNumber; i++) {
-        if ($.inArray(i, skipThese) == -1) $('#selectListID').append("<option value=\"" + i + "\">" + i + "</option>");
-    }
+            $("#timer").text("Business time spent: " + hours + ":" + minutes + ":" + seconds);
+        },
+        init = function() {
+            for (var i = minNumber; i <= maxNumber; i++) {
+                if ($.inArray(i, skipThese) == -1) $('#selectListID').append("<option value=\"" + i + "\">" + i + "</option>");
+            }
+            updateTimer();
+            setInterval(updateTimer, 1000);
+        };
+    init();
+    $('#selectListID').change(function () {
+        people = parseInt($("#selectListID option:selected").text());
+    });
+    $('#resetButton').click(function () {
+        secondsRaw = 0;
+    });
 });
-$('#selectListID').change(function () {
-    people = parseInt($("#selectListID option:selected").text());
-});
-var secondsRaw = 0;
-var people = 1;
-function updateTimer() {
-  secondsRaw = secondsRaw + people;
-
-  var hours = Math.floor(secondsRaw / (60 * 60));
-  
-  var divisor_for_minutes = secondsRaw % (60 * 60);
-  var minutes = Math.floor(divisor_for_minutes / 60);
- 
-  var divisor_for_seconds = divisor_for_minutes % 60;
-  var seconds = Math.ceil(divisor_for_seconds);  
-
-  document.getElementById("timer").firstChild.nodeValue = "business time spent: " + hours + ":" + minutes + ":" + seconds +  " people: ";
-}
 
 function updateClock ( )
 {
-  var currentTime = new Date ( );
+    var currentTime = new Date ( );
+    var currentHours = currentTime.getHours ( );
+    var currentMinutes = currentTime.getMinutes ( );
+    var currentSeconds = currentTime.getSeconds ( );
 
-  var currentHours = currentTime.getHours ( );
-  var currentMinutes = currentTime.getMinutes ( );
-  var currentSeconds = currentTime.getSeconds ( );
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
-  // Pad the minutes and seconds with leading zeros, if required
-  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+    // Choose either "AM" or "PM" as appropriate
+    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
 
-  // Choose either "AM" or "PM" as appropriate
-  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+    // Convert the hours component to 12-hour format if needed
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
 
-  // Convert the hours component to 12-hour format if needed
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+    // Convert an hours component of "0" to "12"
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
-  // Convert an hours component of "0" to "12"
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+    // Compose the string for display
+    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
 
-  // Compose the string for display
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-  // Update the time display
-  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+    // Update the time display
+    document.getElementById("clock").firstChild.nodeValue = currentTimeString;
 }
 
