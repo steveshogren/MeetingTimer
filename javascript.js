fc0a677ff575e56432c1c0eba5fc8dba072a2273
Steve <madklowns@gmail.com>
2012-12-08 09:57:44 -0500

    adding dollars

--- javascript.js
+++ javascript.js
@@ -7,15 +7,20 @@ $(document).ready(function() {
         roundToTens = function(num) {
             return (num < 10 ? "0" : "") + num;
         },
+        roundTo = function(number, to) {
+            return Math.round(number * to) / to;
+        },
         updateTimer = function() {
             secondsRaw = secondsRaw + people;
             var hours = Math.floor(secondsRaw / (60 * 60)),
-                divisor_for_minutes = roundToTens(secondsRaw % (60 * 60)),
-                minutes = roundToTens(Math.floor(divisor_for_minutes / 60)),
-                divisor_for_seconds = roundToTens(divisor_for_minutes % 60),
-                seconds = roundToTens(Math.ceil(divisor_for_seconds));
+            divisor_for_minutes = roundToTens(secondsRaw % (60 * 60)),
+            minutes = roundToTens(Math.floor(divisor_for_minutes / 60)),
+            divisor_for_seconds = roundToTens(divisor_for_minutes % 60),
+            seconds = roundToTens(Math.ceil(divisor_for_seconds)),
+            dollars = roundTo(.011 * secondsRaw, 100);
+            
 
-            $("#timer").text("Business time spent: " + hours + ":" + minutes + ":" + seconds);
+            $("#timer").text("Total time: " + hours + ":" + minutes + ":" + seconds + " dollars: " + dollars);
         },
         init = function() {
             for (var i = minNumber; i <= maxNumber; i++) {
